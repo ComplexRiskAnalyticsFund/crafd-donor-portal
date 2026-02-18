7e0b0285ed1bc5792c8f7a10b5b7b1940219434b
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2026-02-18 09:59:16 -0500

    env; partners

---
+++ src/app/partners/page.tsx
@@ -0,0 +1,9 @@
+export default function PartnersPage() {
+  return (
+    <iframe
+      src="/crafd-partners.pdf"
+      className="fixed inset-0 h-full w-full border-none"
+      title="CRAF'd Partners"
+    />
+  );
+}
