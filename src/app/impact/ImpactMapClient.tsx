0f13c233108746a9ca71fefc71d4d6af2aad5410
hushbrush <harshitachakravadhanula@gmail.com>
2026-05-14 10:52:55 -0400

    desktop map data+mobile v1

---
+++ src/app/impact/ImpactMapClient.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import type { CrafdProject } from "@/types";
+
+const ImpactMap = dynamic(() => import("./ImpactMap"), { ssr: false });
+
+interface Props {
+  projects: CrafdProject[];
+  variant?: "flat" | "density";
+}
+
+export default function ImpactMapClient(props: Props) {
+  return <ImpactMap {...props} />;
+}
